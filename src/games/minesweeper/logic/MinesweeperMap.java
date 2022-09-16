83b4a13c672d77146762bbea3b1732f54522eb10
brianalcl <brianclemente@outlook.com>
2022-09-16 12:34:09 -0300

    more work was done on the minesweeper

--- src/games/minesweeper/logic/MinesweeperMap.java
+++ src/games/minesweeper/logic/MinesweeperMap.java
@@ -11,8 +11,9 @@ public class MinesweeperMap extends Map{
 	protected MinesweeperCell[][]matrix;
 	protected MinesweeperGame game;
 	protected GraphicCell flag;
-	protected GraphicCell bomb;
 	protected GraphicCell empty;
+	protected int size;
+	protected int bombs;
 	
 	public MinesweeperMap(MinesweeperGame game) {
 		super(game, false);
@@ -20,21 +21,23 @@ public class MinesweeperMap extends Map{
 		this.rnd = NRandom.getInstance();
 		this.matrix = new MinesweeperCell[COLUMN][ROW];
 		this.flag = new GraphicCell(this.game.getImageFactory().getFlag(), this.game.getImageFactory().getColorDefault());
-		this.bomb = new GraphicCell(this.game.getImageFactory().getBomb(), this.game.getImageFactory().getColorDefault());
 		freeCell = new GraphicCell(null, this.game.getImageFactory().getColorDefault());
-		this.empty = new GraphicCell(null, null);
+		this.empty = new GraphicCell(game.getImageFactory().getEmpty(), game.getImageFactory().getColorEmpty());
+		this.bombs = 15;
+		this.size = (matrix.length - 2) * matrix[0].length - bombs;
 		
 		for(int r = 0; r < matrix.length; r++) 
 			for(int c = 0; c < matrix[0].length; c++) {
 				matrix[r][c] = new MinesweeperCell(r, c,this);
 				matrix[r][c].clear();
 			}
-		for(int r = 0; r < 7; r++) 
+		
+		for(int r = 0; r < 2; r++) 
 			for(int c = 0; c < matrix[0].length; c++) {
 				matrix[r][c].setUneditable();
 				matrix[r][c].put(empty);
 			}
-		charge(10);
+		charge(bombs);
 	}
 	
 	private void charge(int count) {
@@ -43,14 +46,13 @@ public class MinesweeperMap extends Map{
 		int p = 0;
 		
 		while(p < count) {
-				r = Math.abs(rnd.nextInt()%9) + 7;
-				c = Math.abs(rnd.nextInt()%9);
-				if(!matrix[r][c].isBomb()) {
-					matrix[r][c].setBomb();
-					matrix[r][c].put(bomb);
-					setNumber(matrix[r][c]);
-					p++;
-				}
+			r = Math.abs(rnd.nextInt() % (matrix.length - 2)) + 2;
+			c = Math.abs(rnd.nextInt() % matrix[0].length);
+			if(!matrix[r][c].isBomb()) {
+				matrix[r][c].setBomb();
+				setNumber(matrix[r][c]);
+				p++;
+			}
 		}
 	}
 	
@@ -58,74 +60,54 @@ public class MinesweeperMap extends Map{
 		int r = 0;
 		int c = 0;
 		
-		GraphicCell gc = new GraphicCell(null, game.getImageFactory().getColorDefault());
-		
 		r = cell.getRow() - 1;
 		c = cell.getColumn();
-		if(r >= 7 && !matrix[r][c].isBomb()) {
+		if(r >= 2 && !matrix[r][c].isBomb()) {
 			matrix[r][c].setNumber(matrix[r][c].getNumber()+1);
-			gc.setText(matrix[r][c].getNumber()+"");
-			matrix[r][c].put(gc);
 		}
 			
 		r = cell.getRow() + 1;
 		c = cell.getColumn();
 		if(r <= 15 && !matrix[r][c].isBomb()){
 			matrix[r][c].setNumber(matrix[r][c].getNumber()+1);
-			gc.setText(matrix[r][c].getNumber()+"");
-			matrix[r][c].put(gc);
 		}
 		
 		r = cell.getRow();
 		c = cell.getColumn() - 1;
 		if(c >= 0 && !matrix[r][c].isBomb()){
 			matrix[r][c].setNumber(matrix[r][c].getNumber()+1);
-			gc.setText(matrix[r][c].getNumber()+"");
-			matrix[r][c].put(gc);
 		}
 		
 		r = cell.getRow();
 		c = cell.getColumn() + 1;
 		if(c <= 8 && !matrix[r][c].isBomb()){
 			matrix[r][c].setNumber(matrix[r][c].getNumber()+1);
-			gc.setText(matrix[r][c].getNumber()+"");
-			matrix[r][c].put(gc);
 		}
 		
 		r = cell.getRow() - 1;
 		c = cell.getColumn() - 1;
-		if(r >= 7 && c >= 0 && !matrix[r][c].isBomb()){
+		if(r >= 2 && c >= 0 && !matrix[r][c].isBomb()){
 			matrix[r][c].setNumber(matrix[r][c].getNumber()+1);
-			gc.setText(matrix[r][c].getNumber()+"");
-			matrix[r][c].put(gc);
 		}
 		
 		r = cell.getRow() + 1;
 		c = cell.getColumn() + 1;
 		if(r <= 15 && c <= 8 && !matrix[r][c].isBomb()){
 			matrix[r][c].setNumber(matrix[r][c].getNumber()+1);
-			gc.setText(matrix[r][c].getNumber()+"");
-			matrix[r][c].put(gc);
 		}
 		
 		r = cell.getRow() - 1;
 		c = cell.getColumn() + 1;
-		if(c <= 8 && r >= 7 && c >= 0 && !matrix[r][c].isBomb()){
+		if(c <= 8 && r >= 2 && c >= 0 && !matrix[r][c].isBomb()){
 			matrix[r][c].setNumber(matrix[r][c].getNumber()+1);
-			gc.setText(matrix[r][c].getNumber()+"");
-			matrix[r][c].put(gc);
 		}
 		
 		r = cell.getRow() + 1;
 		c = cell.getColumn() - 1;
 		if(c >= 0 && r <= 15 && c <= 8 && !matrix[r][c].isBomb()){
 			matrix[r][c].setNumber(matrix[r][c].getNumber()+1);
-			gc.setText(matrix[r][c].getNumber()+"");
-			matrix[r][c].put(gc);
 		}
 	}
-	
-	
 	
 	public void changeCell(MinesweeperCell minesweeperCell) {
 		game.changeCell(minesweeperCell);
@@ -139,19 +121,19 @@ public class MinesweeperMap extends Map{
 				show(row, column);
 			}
 		}
+		if(size == 0)
+			win();
 	}
 	
 	public void show(int row, int column) {
-		
 		GraphicCell gc = new GraphicCell(null, this.game.getImageFactory().getColorDefault().brighter());
 		mark(row, column, gc);
-		
 	}
 
 	private void mark(int row, int column, GraphicCell gc) { //TODO I don't know if there is a more efficient way
-		gc.setText("");
-
-		if((row >= 7 && row <= 15 && column >= 0 && column <= 8) && matrix[row][column].isEditable()) {
+		if((row >= 2 && row <= 15 && column >= 0 && column <= 8) && matrix[row][column].isEditable()) {
+			gc.setText("");
+			size--;
 			if(matrix[row][column].getNumber() != 0) {
 				gc.setText(matrix[row][column].getNumber()+"");
 				matrix[row][column].setUneditable();
@@ -160,45 +142,19 @@ public class MinesweeperMap extends Map{
 			else {
 				matrix[row][column].setUneditable();
 				matrix[row][column].put(gc);
+				gc.setText("");
+				
 				mark(row - 1, column - 1, gc);
-				gc.setText("");
-				
-				matrix[row][column].setUneditable();
-				matrix[row][column].put(gc);
 				mark(row + 1, column + 1, gc);
-				gc.setText("");
-				
-				matrix[row][column].setUneditable();
-				matrix[row][column].put(gc);
 				mark(row - 1, column + 1, gc);
-				gc.setText("");
-				
-				matrix[row][column].setUneditable();
-				matrix[row][column].put(gc);
 				mark(row + 1, column - 1, gc);
-				gc.setText("");
-				
-				matrix[row][column].setUneditable();
-				matrix[row][column].put(gc);
 				mark(row, column + 1, gc);
-				gc.setText("");
-				
-				matrix[row][column].setUneditable();
-				matrix[row][column].put(gc);
 				mark(row + 1, column, gc);
-				gc.setText("");
-				
-				matrix[row][column].setUneditable();
-				matrix[row][column].put(gc);
 				mark(row, column - 1, gc);
-				gc.setText("");
-				
-				matrix[row][column].setUneditable();
-				matrix[row][column].put(gc);
 				mark(row - 1, column, gc);
-				gc.setText("");
 			}
 		}
+
 	}
 
 	public void putFlag(int row, int column) {
